208e74661063b5ea8ac24b0b4c2d37ec37bfe74d
Degz84 <88509664+Degz84@users.noreply.github.com>
2021-08-06 22:01:27 +0100

    Add files via upload

---
+++ 24 Hour report on event ID 19.ps1
@@ -0,0 +1,6 @@
+﻿$StartTime = "05/08/2021 8:00:01 AM"
+$EndTime = "06/08/2021 8:00:01 AM"
+$Results = Get-WinEvent -FilterHashTable @{LogName="System"; ID=19; StartTime=$StartTime; EndTime=$EndTime;}
+ForEach($Result in $Results){
+$Result.TimeCreated,$Result.LogName,$Result.ProviderName,$Result.Id,$Result.LevelDisplayName}
+$Results | Export-Csv -LiteralPath "C:\Temp\system.csv"
